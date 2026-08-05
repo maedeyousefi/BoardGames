19acb327cbfd43aef7afff4353be2cfb88ac39a4
maedeyousefi <maedeyousefi2081@gmail.com>
2026-08-05 18:48:35 +0330

    Add effect for buttons

---
+++ Assets/Scripts/UI/ButtonHoverEffect.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Vector3 originalScale;
+
+    [SerializeField] private float hoverScale = 1.05f;
+
+    private void Awake()
+    {
+        originalScale = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale = originalScale * hoverScale;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = originalScale;
+    }
+}
